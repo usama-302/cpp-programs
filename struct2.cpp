5b2e925a7e668b83798d954199789b835c9fbeb4
unknown <you@example.com>
2021-12-11 01:27:22 -0800

    files

---
+++ struct2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct student
+{
+    string name;
+    int roll;
+    int id;
+    
+};
+ int main()
+ {
+ 	student g1,g2;
+ 	g1.name="usama";
+ 	
+ 	g1.roll=1344;
+ 	
+ 	g1.id=56;
+ 	
+ 	cout<<"Display g1 data"<<endl<<"name :"<<g1.name<<endl<<"roll :"<<g1.roll<<endl<<"id :"<<g1.id<<endl;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
